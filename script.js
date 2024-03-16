48c22367b63d76e7d630e1e3749bb8e784a2b78b
Ian <ian.nptk@gmail.com>
2024-03-16 17:56:29 +0100

    Functions to buttons, fix stacked event listeners

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const container = document.querySelector("#main-container");
 const canvasSizeInput = document.querySelector("#canvas-size");
 const normalButton = document.querySelector("#normal");
 const psychButton = document.querySelector("#psychedelic");
-// normalButton.disabled = true;
 const clearButton = document.querySelector("#clear");
-let rangeInputEvent = new Event("input");
 const rgbArray = [];
+
+let rangeInputEvent = new Event("input");
+let isNormal = true;
 
 function createDivs(nbrDivs = 16) {
   const totalDivs = nbrDivs ** 2 * 1.5;
@@ -18,18 +19,70 @@ function createDivs(nbrDivs = 16) {
     div.style.width = 40 / nbrDivs + "vw";
     div.style.height = 40 / nbrDivs + "vw";
     div.id = i;
-    div.addEventListener("mouseenter", function (event) {
-      let rgbVal = darkenSquare(i);
-      div.style.backgroundColor = `rgb(${rgbVal}, ${rgbVal}, ${rgbVal})`;
-    });
+
+    if (!isNormal) {
+      div.addEventListener("mouseenter", function (event) {
+        div.style.backgroundColor = `rgb(${Math.floor(
+          Math.random() * 256
+        )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+          Math.random() * 256
+        )})`;
+      });
+    } else {
+      div.addEventListener("mouseenter", function (event) {
+        let rgbVal = darkenSquare(i);
+        div.style.backgroundColor = `rgb(${rgbVal}, ${rgbVal}, ${rgbVal})`;
+      });
+    }
 
     divArray.push(div);
     rgbArray.push(255);
   }
 
+  // changeMode();
   for (const div of divArray) {
     container.appendChild(div);
   }
+}
+
+function normalEvent(event) {
+  let div = event.target;
+  let rgbVal = darkenSquare(div.id);
+  div.removeEventListener("mouseenter", normalEvent);
+  div.removeEventListener("mouseenter", psychedelicEvent);
+  div.style.backgroundColor = `rgb(${rgbVal}, ${rgbVal}, ${rgbVal})`;
+}
+
+function psychedelicEvent(event) {
+  let div = event.target;
+  div.removeEventListener("mouseenter", normalEvent);
+  div.removeEventListener("mouseenter", psychedelicEvent);
+  div.style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+}
+
+function changeMode() {
+  if (!isNormal) {
+    for (const div of container.children) {
+      div.addEventListener("mouseenter", function (event) {
+        div.style.backgroundColor = `rgb(${Math.floor(
+          Math.random() * 256
+        )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+          Math.random() * 256
+        )})`;
+      });
+    }
+  } else {
+    for (const div of container.children) {
+      div.addEventListener("mouseenter", function (event) {
+        let rgbVal = darkenSquare(div.id);
+        div.style.backgroundColor = `rgb(${rgbVal}, ${rgbVal}, ${rgbVal})`;
+      });
+    }
+  }
+  // isNormal = !isNormal;
+  // !isNormal;
 }
 
 function removeDivs() {
@@ -42,17 +95,12 @@ function darkenSquare(id) {
   return (rgbArray[id] -= 25.5);
 }
 
-canvasSizeInput.addEventListener("input", function () {
-  const value = canvasSizeInput.value;
-  removeDivs();
-  createDivs(value);
-  console.log(value);
-});
-
 normalButton.addEventListener("click", function () {
   normalButton.classList.add("selected");
   psychButton.classList.remove("selected");
 
+  isNormal = true;
+  changeMode();
   // normalButton.disabled = true;
   // psychButton.disabled = false;
 });
@@ -61,6 +109,8 @@ psychButton.addEventListener("click", function () {
   psychButton.classList.add("selected");
   normalButton.classList.remove("selected");
 
+  isNormal = false;
+  changeMode();
   // normalButton.disabled = false;
   // psychButton.disabled = true;
 });
@@ -69,6 +119,14 @@ clearButton.addEventListener("click", function () {
   canvasSizeInput.dispatchEvent(rangeInputEvent);
 });
 
+canvasSizeInput.addEventListener("input", function () {
+  const value = canvasSizeInput.value;
+  removeDivs();
+  createDivs(value);
+  console.log(value);
+});
+
 //
 
 createDivs();
+// changeMode();
