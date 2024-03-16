942b07ebc8c9008bfd73abefd2a4e533c99b6687
Ian <ian.nptk@gmail.com>
2024-03-16 06:58:10 +0100

    added individual event listeners and RGB array

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 let container = document.querySelector("#main-container");
 const divClass = document.querySelector(".square");
-// let div = document.createElement("div");
+const rgbArray = [];
 
 function createDivs(nbrDivs = 16) {
-  const totalDivs = nbrDivs ** 2;
-  const divsArray = [];
+  const totalDivs = nbrDivs ** 2 * 1.5;
+  const divArray = [];
   //   divClass.style.width = "calc(100% / " + nbrDivs + ");";
   //   divClass.style.height = nbrDivs + "px";
 
   for (let i = 0; i < totalDivs; i++) {
-    let j = i;
+    // let j = i;
     let div = document.createElement("div");
 
     div.classList.add("square");
-    div.textContent = ++j;
+    div.style.width = 40 / nbrDivs + "vw";
+    div.style.height = 40 / nbrDivs + "vw";
+    div.id = i;
+    div.addEventListener("mouseenter", function (event) {
+      let rgbVal = darkenSquare(i);
+      div.style.backgroundColor = `rgb(${rgbVal}, ${rgbVal}, ${rgbVal})`;
+    });
 
-    divsArray.push(div);
+    divArray.push(div);
+    rgbArray.push(255);
   }
 
-  for (const div of divsArray) {
+  for (const div of divArray) {
     container.appendChild(div);
   }
+}
+
+function darkenSquare(id) {
+  return (rgbArray[id] -= 25.5);
 }
 
 // divClass.addEventListener("hover", function (event) {
