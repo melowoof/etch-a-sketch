0c67ffb730062fe95eccf71fff2641f30d9e9665
Ian <ian.nptk@gmail.com>
2024-03-16 11:25:25 +0100

    Added eventListeners to buttons, modified appearances
    TODO: add psychedelic mode, possibly moving divs array out of createDivs function

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let container = document.querySelector("#main-container");
-const divClass = document.querySelector(".square");
+const container = document.querySelector("#main-container");
+const canvasSizeInput = document.querySelector("#canvas-size");
+const normalButton = document.querySelector("#normal");
+const psychButton = document.querySelector("#psychedelic");
+// normalButton.disabled = true;
+const clearButton = document.querySelector("#clear");
+let rangeInputEvent = new Event("input");
 const rgbArray = [];
 
 function createDivs(nbrDivs = 16) {
   const totalDivs = nbrDivs ** 2 * 1.5;
   const divArray = [];
-  //   divClass.style.width = "calc(100% / " + nbrDivs + ");";
-  //   divClass.style.height = nbrDivs + "px";
 
   for (let i = 0; i < totalDivs; i++) {
-    // let j = i;
     let div = document.createElement("div");
 
     div.classList.add("square");
@@ -30,8 +32,43 @@ function createDivs(nbrDivs = 16) {
   }
 }
 
+function removeDivs() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
 function darkenSquare(id) {
   return (rgbArray[id] -= 25.5);
 }
+
+canvasSizeInput.addEventListener("input", function () {
+  const value = canvasSizeInput.value;
+  removeDivs();
+  createDivs(value);
+  console.log(value);
+});
+
+normalButton.addEventListener("click", function () {
+  normalButton.classList.add("selected");
+  psychButton.classList.remove("selected");
+
+  // normalButton.disabled = true;
+  // psychButton.disabled = false;
+});
+
+psychButton.addEventListener("click", function () {
+  psychButton.classList.add("selected");
+  normalButton.classList.remove("selected");
+
+  // normalButton.disabled = false;
+  // psychButton.disabled = true;
+});
+
+clearButton.addEventListener("click", function () {
+  canvasSizeInput.dispatchEvent(rangeInputEvent);
+});
+
+//
 
 createDivs();
